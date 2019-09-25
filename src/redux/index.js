4731a2a521c3f796050ea2f9342149fb5868d5c3
Saul M <fob2257@gmail.com>
2019-09-25 11:13:56 -0700

    Removed redux logger for production build

--- src/redux/index.js
+++ src/redux/index.js
@@ -8,7 +8,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import rootReducer from './reducers';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const persistedRootReducer = persistReducer({
   key: 'root',
