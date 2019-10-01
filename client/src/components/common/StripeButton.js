c6acefa07eae3660b1b1c4ee12b3d7524ce11843
Saul M <fob2257@gmail.com>
2019-10-01 12:54:14 -0700

    Moved react app into client folder. Added express server on index. Removed create-react-app buildpack from heroku

--- client/src/components/common/StripeButton.js
+++ client/src/components/common/StripeButton.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 import { default as stripeConfig } from '../../stripe.config.json';
 
 const StripeButton = ({ price }) => {
   const priceCents = price * 100;
 
-  const onToken = (token) => {
-    console.log(token);
+  const onToken = async (token) => {
+    try {
+      // console.log(token);
+      const res = await axios.post('payment', { token, amount: priceCents });
+
+      console.log(res);
+      alert('Payment successful');
+    } catch (error) {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('There was an issue with your payment');
+    }
   };
 
   return (
---
+++ server.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const express = require('express');
+const compression = require('compression');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const app = express();
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan('common'));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  });
+}
+
+const stripeCharge = (body = {}) =>
+  new Promise((resolve, reject) => {
+    stripe.charges.create(body, (error, charge) => {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(charge);
+    });
+  });
+
+app.post('/payment', async (req, res) => {
+  try {
+    const {
+      amount,
+      currency = 'usd',
+      token: { id: source },
+    } = req.body;
+
+    const charge = await stripeCharge({ source, amount, currency });
+
+    res.status(200).send(charge);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+http.createServer(app).listen(port, host, () => {
+  console.log(`crwn-clothing-server listening on http://${host}:${port}`);
+});
