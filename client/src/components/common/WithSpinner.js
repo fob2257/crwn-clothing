2a8cecfc30e7379800bbbeb09b6a84f27b5c2855
Saul M <fob2257@gmail.com>
2019-10-02 13:57:07 -0700

    Change WithSpinner component behaviour

--- client/src/components/common/WithSpinner.js
+++ client/src/components/common/WithSpinner.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 import { SpinnerContainer, SpinnerOverlay } from './WithSpinner.styles';
 
-const WithSpinner = WrappedComponent => ({ isLoading, ...props }) =>
-  isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  )
-    : (<WrappedComponent {...props} />);
+const WithSpinner = () => (
+  <SpinnerOverlay>
+    <SpinnerContainer />
+  </SpinnerOverlay>
+);
 
 export default WithSpinner;
